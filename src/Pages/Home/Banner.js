2551aefce9a71b7e8680926a4aa1ee73b6239835
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-23 23:30:40 +0600

    banner design updated- animation on scroll

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="hero h-[600px] bg-[url('https://i.ibb.co/17yv998/banner-small.jpg')] md:bg-right-top md:bg-[url('https://i.ibb.co/zmR1bGH/banner-lg.jpg')]" >
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
+            <div data-aos="fade-up" className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
                     <p className="mb-5">Tools factory is electric tools manufacuring company that builds sustainable and long lasting tools. With over 5000+ customer world wide, we are gowing rapidly... </p>

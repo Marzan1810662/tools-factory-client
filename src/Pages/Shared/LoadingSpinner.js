a38223d7655aaf9f1832437f4d71360788898808
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-26 02:52:25 +0600

    loading spinner design edited

--- src/Pages/Shared/LoadingSpinner.js
+++ src/Pages/Shared/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = () => {
     return (
-            <div className="flex items-center justify-center space-x-2 animate-pulse">
+            <div className="h-screen flex items-center justify-center space-x-2 animate-pulse">
                 <div className="w-8 h-8 bg-primary rounded-full"></div>
                 <div className="w-8 h-8 bg-primary rounded-full"></div>
                 <div className="w-8 h-8 bg-primary rounded-full"></div>
